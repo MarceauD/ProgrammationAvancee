b0fb135808a6423404e829295a164c9f8fb82fa1
MarceauD <dida.marceau@gmail.com>
2016-09-29 14:54:50 +0200

    Ajout d'un ficher contenant toutes les constantes du programme

---
+++ defines.h
@@ -0,0 +1,16 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define SCREEN_TITLE "GAME_TITLE"
+
+#define SCREEN_WIDTH 0
+#define SCREEN_HEIGHT 0
+#define SPRITE_SIZE 0
+ 
+
+
+
+
+
+
+#endif //DEFINES_H
