1c8d484594445f079c1179e7d829969f98dc380c
vample <vample@live.fr>
2016-10-12 01:05:40 +0200

    Ajout de quitter et de quelques commentaires

--- gamestate.h
+++ gamestate.h
@@ -20,39 +20,39 @@ struct Time{
 typedef struct Time Time;
 
 /*GAMESTATE OPERATIONS*/
-/*initialise a GameState structure*/
-extern GameState init_GameState(); 
+    /*initialise a GameState structure*/
+    extern GameState init_GameState(); 
 
-/*returns true if the program should end*/
-extern bool isOver(GameState gameState); 
+    /*returns true if the program should end*/
+    extern bool isOver(GameState gameState); 
 
-/*returns true if the program should quit the menu*/
-extern bool isinMenu(GameState gameState);
+    /*returns true if the program should quit the menu*/
+    extern bool isinMenu(GameState gameState);
 
-/*set the EndProgram variable*/
-extern GameState write_EndProgramStatus(bool B, GameState gameState);
+    /*set the EndProgram variable*/
+    extern GameState write_EndProgramStatus(bool B, GameState gameState);
 
-/*set the inMenu variable*/
-extern GameState write_MenuStatus(bool B, GameState gameState);
+    /*set the inMenu variable*/
+    extern GameState write_MenuStatus(bool B, GameState gameState);
 
-/*returns the EndProgram status*/
-extern bool read_ProgramStatus(GameState gameState);
+    /*returns the EndProgram status*/
+    extern bool read_ProgramStatus(GameState gameState);
 
-/*returns the inMenu status*/
-extern bool read_MenuStatus(GameState gameState);
+    /*returns the inMenu status*/
+    extern bool read_MenuStatus(GameState gameState);
 
 /*TIME OPERATIONS*/
-/*initialise a time structure*/
-extern Time init_Time(); 
+    /*initialise a time structure*/
+    extern Time init_Time(); 
 
-/*returns the gap between the current time and the previous time*/
-extern int time_gap(Time T);
+    /*returns the gap between the current time and the previous time*/
+    extern int time_gap(Time T);
 
-/*update the previous time, put the current time to the previous time*/
-extern void update_previousTime(Time *T);
+    /*update the previous time, put the current time to the previous time*/
+    extern void update_previousTime(Time *T);
 
-/*updates the current time with SDL_GetTicks()*/
-extern void update_currentTime(Time *T);
+    /*updates the current time with SDL_GetTicks()*/
+    extern void update_currentTime(Time *T);
 
 
 #endif //GAMESTATE_H
--- graphics.c
+++ graphics.c
@@ -1,9 +1,10 @@
 #include "graphics.h"
-
+/*load BMP sprite*/
 SDL_Surface* loadImage(SDL_Surface* sprite,char* file){
         SDL_Surface* temp;
         temp = SDL_LoadBMP(file);
         
+        /*show message error if sprite cant be found*/
         if (!temp){
                 printf("Unable to load the BMP image%s\n",SDL_GetError());
                 return NULL;
@@ -16,30 +17,34 @@ SDL_Surface* loadImage(SDL_Surface* sprite,char* file){
 }
 
 
-
+/*fighter structure*/
 fighter init_fighter(){
         fighter F;
-
+        
+        /* load sprite*/
         F.sprite = loadImage(F.sprite,"sprite_fighter.bmp");
-
+        
+        /* setup sprite colorkey and turn on RLE */
         SDL_SetColorKey(F.sprite,SDL_SRCCOLORKEY, SDL_MapRGB(F.sprite->format,49,82,49));
 
+        /*sprite's rectangle position*/
         F.source.x = 0;
         F.source.y = 0;
         F.source.h = SPRITE_HEIGHT;
         F.source.w = SPRITE_WIDTH;
 
+        /*sprite's position in the game*/
         F.rcSprite.x = DEFAULT_SPRITE_POSITION_X;
         F.rcSprite.y = DEFAULT_SPRITE_POSITION_Y;
         F.rcSprite.h = DEFAULT_SPRITE_HEIGHT;
         F.rcSprite.w = DEFAULT_SPRITE_WIDTH;
 
+        /*lifepoints and damagepoints*/
         F = write_lifepoints(DEFAULT_LIFEPOINTS,F);
-
         F = write_damage(DEFAULT_HITDAMAGE,F);
 
+        /*position : direction and standing/kneeling*/
         F.r = RIGHT;
-
         F.p = STANDING;
         
         //EN ATTENTE D'UNE MEILLEURE DEFINITION
@@ -49,31 +54,34 @@ fighter init_fighter(){
 
 }
 
-bool isAlive(fighter F){
-        return read_lifepoints(F) == 0;
-}
+/*Used fonctions in the fighter*/
+    /*tests if there is lifepoints, returns 0 if it is alive */
+    bool isAlive(fighter F){
+            return read_lifepoints(F) == 0;
+    }
+    
+    /*reading fonctions*/
+    int read_lifepoints(fighter F){
+            return F.lifepoints;
+    }
+    int read_damage(fighter F){
+            return F.damage;
+    }
 
-int read_lifepoints(fighter F){
-        return F.lifepoints;
-}
-
-int read_damage(fighter F){
-        return F.damage;
-}
-
-fighter write_lifepoints(int lifepoints, fighter F){
-        F.lifepoints = lifepoints;;
-        return F;
-}
-
-fighter write_damage(int damage, fighter F){
-        F.damage = damage;
-        return F;
-}
-
-void FreeFighter(fighter F){
-        SDL_FreeSurface(F.sprite);
-}
+    /*writing fonctions*/
+    fighter write_lifepoints(int lifepoints, fighter F){
+            F.lifepoints = lifepoints;;
+            return F;
+    }
+    fighter write_damage(int damage, fighter F){
+            F.damage = damage;
+            return F;
+    }
+    
+    /*clean up fighter*/
+    void FreeFighter(fighter F){
+            SDL_FreeSurface(F.sprite);
+    }
 
 
 
